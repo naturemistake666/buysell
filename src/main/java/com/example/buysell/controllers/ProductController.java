d774f76ec781efd64d0480f449f579203827e867
naturemistake666 <65819256+naturemistake666@users.noreply.github.com>
2022-11-07 17:20:43 +0300

    First commit

---
+++ src/main/java/com/example/buysell/controllers/ProductController.java
@@ -0,0 +1,63 @@
+package com.example.buysell.controllers;
+
+import com.example.buysell.models.Product;
+import com.example.buysell.models.User;
+import com.example.buysell.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.Principal;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+
+    @GetMapping("/")
+    public String products(@RequestParam(name = "searchWord", required = false) String title, Model model, Principal principal) {
+        model.addAttribute("products", productService.listProduct(title));
+        model.addAttribute("user", productService.getUserByPrincipal(principal));
+        model.addAttribute("searchWord", title);
+        return "products";
+    }
+
+    @PostMapping("/product/create")
+    public String createProduct(@RequestParam("file1") MultipartFile file1, @RequestParam("file2") MultipartFile file2,
+                                @RequestParam("file3") MultipartFile file3, Product product, Principal principal) throws IOException {
+        productService.saveProduct(principal, product, file1, file2, file3);
+        return "redirect:/my/products";
+    }
+
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id, Principal principal) {
+        productService.deleteProduct(productService.getUserByPrincipal(principal), id);
+        productService.deleteProduct(id);
+        return "redirect:/my/products";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productIfo(@PathVariable Long id, Model model, Principal principal) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("user", productService.getUserByPrincipal(principal));
+        model.addAttribute("product", product);
+        model.addAttribute("images", product.getImages());
+        model.addAttribute("authorProduct", product.getUser());
+        return "product-info";
+    }
+
+    @GetMapping("/my/products")
+    public String userProducts(Principal principal, Model model) {
+        User user = productService.getUserByPrincipal(principal);
+        model.addAttribute("user", user);
+        model.addAttribute("products", user.getProducts());
+        return "my-products";
+    }
+}
